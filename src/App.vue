e1cfb1717959eebe9062028b35cc597fedd277a0
yue.chang <r3399r@gmail.com>
2022-02-14 11:47:27 +0800

    fix route to meet github page

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,8 @@ import HelloWorld from '@/components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/porrima/">Home</RouterLink>
+        <RouterLink to="/porrima/about">About</RouterLink>
       </nav>
     </div>
   </header>
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,12 +5,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/porrima/',
       name: 'home',
       component: HomeView
     },
     {
-      path: '/about',
+      path: '/porrima/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route

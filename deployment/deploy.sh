718727dec45804a1e34b96f0f42e2b3cfd58a64e
Yue Chang <r3399r@gmail.com>
2022-04-05 14:16:16 +0800

    fix shell script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -12,10 +12,10 @@ echo project: $project
 echo domain: $subDomain.$domain
 echo ====================================================================================
 
-# echo deploy backend AWS...
-# aws cloudformation package --template-file aws/cloudformation/template.yaml --output-template-file packaged.yaml --s3-bucket y-cf-midway
-# aws cloudformation deploy --template-file packaged.yaml --stack-name $project-$env-stack --parameter-overrides TargetEnvr=$env Project=$project SubDomain=$subDomain Domain=$domain --no-fail-on-empty-changeset --s3-bucket y-cf-midway
-# echo ====================================================================================
+echo deploy backend AWS...
+aws cloudformation package --template-file aws/cloudformation/template.yaml --output-template-file packaged.yaml --s3-bucket y-cf-midway
+aws cloudformation deploy --template-file packaged.yaml --stack-name $project-$env-stack --parameter-overrides TargetEnvr=$env Project=$project SubDomain=$subDomain Domain=$domain --no-fail-on-empty-changeset --s3-bucket y-cf-midway
+echo ====================================================================================
 
 echo deploy frontend to S3...
 cd ..

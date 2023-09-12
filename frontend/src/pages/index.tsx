f35572c52f587efb2579e73243dd9e8411a9531e
Yue <yue@potentia.asia>
2023-09-12 12:41:19 +0800

    improve ui

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -36,93 +36,73 @@ const Home = () => {
         />
       </Head>
       <div className="min-h-screen overflow-x-auto bg-red-700 py-5">
-        <div className="mx-auto flex h-[64px] min-w-[1000px] max-w-[1440px] items-center bg-red-500 text-center font-bold text-yellow-50">
+        <div className="mx-auto flex h-[64px] min-w-[1000px] max-w-[1440px] items-center text-center font-bold text-yellow-50">
           <div
-            className="sticky left-0 flex h-full w-1/6 min-w-[150px] cursor-pointer items-center justify-center border-r-[1px] border-r-gray-800 bg-orange-500"
+            className="sticky left-0 box-border flex h-full w-1/6 min-w-[150px] cursor-pointer items-center justify-center border-r-[1px] border-r-gray-800 bg-orange-500 px-1"
             onClick={() => click('id')}
           >
-            <div className="p-1">
-              刮刮樂主題{sort === 'id' && <span>{order === 'asc' ? '↓' : '↑'}</span>}
-            </div>
+            刮刮樂主題{sort === 'id' && <span>{order === 'asc' ? '↓' : '↑'}</span>}
           </div>
           <div
-            className="flex h-full w-1/12 min-w-[75px] cursor-pointer items-center justify-center bg-orange-500"
+            className="box-border flex h-full w-1/12 min-w-[75px] cursor-pointer items-center justify-center bg-orange-500 px-1"
             onClick={() => click('price')}
           >
-            <div className="p-1">
-              售價{sort === 'price' && <span>{order === 'asc' ? '↓' : '↑'}</span>}
-            </div>
+            售價{sort === 'price' && <span>{order === 'asc' ? '↓' : '↑'}</span>}
           </div>
           <div
-            className="flex h-full w-1/12 min-w-[150px] cursor-pointer items-center justify-center bg-orange-500"
+            className="box-border flex h-full w-1/12 min-w-[150px] cursor-pointer items-center justify-center bg-orange-500 px-1"
             onClick={() => click('totalW')}
           >
-            <div className="p-1">
-              發行數量{sort === 'totalW' && <span>{order === 'asc' ? '↓' : '↑'}</span>}
-            </div>
+            發行數量{sort === 'totalW' && <span>{order === 'asc' ? '↓' : '↑'}</span>}
           </div>
           <div
-            className="flex h-full w-1/12 min-w-[75px] cursor-pointer items-center justify-center bg-orange-900"
+            className="box-border flex h-full w-1/12 min-w-[75px] cursor-pointer items-center justify-center bg-orange-900 px-1"
             onClick={() => click('bingoRate')}
           >
-            <div className="p-1">
-              中獎率{sort === 'bingoRate' && <span>{order === 'asc' ? '↓' : '↑'}</span>}
-            </div>
+            中獎率{sort === 'bingoRate' && <span>{order === 'asc' ? '↓' : '↑'}</span>}
           </div>
           <div
-            className="flex h-full w-1/12 min-w-[75px] cursor-pointer items-center justify-center bg-red-400"
+            className="box-border flex h-full w-1/12 min-w-[75px] cursor-pointer items-center justify-center bg-red-400 px-1"
             onClick={() => click('winRate')}
           >
-            <div className="p-1">
-              勝率{sort === 'winRate' && <span>{order === 'asc' ? '↓' : '↑'}</span>}
-            </div>
+            勝率{sort === 'winRate' && <span>{order === 'asc' ? '↓' : '↑'}</span>}
           </div>
           <div
-            className="flex h-full w-1/12 min-w-[75px] cursor-pointer items-center justify-center bg-red-400"
+            className="box-border flex h-full w-1/12 min-w-[75px] cursor-pointer items-center justify-center bg-red-400 px-1"
             onClick={() => click('noLoseRate')}
           >
-            <div className="p-1">
-              回本率{sort === 'noLoseRate' && <span>{order === 'asc' ? '↓' : '↑'}</span>}
-            </div>
+            回本率{sort === 'noLoseRate' && <span>{order === 'asc' ? '↓' : '↑'}</span>}
           </div>
           <div
-            className="flex h-full w-1/12 min-w-[75px] cursor-pointer items-center justify-center bg-red-400"
+            className="box-border flex h-full w-1/12 min-w-[75px] cursor-pointer items-center justify-center bg-red-400 px-1"
             onClick={() => click('expect')}
           >
-            <div className="p-1">
-              每百元期望值{sort === 'expect' && <span>{order === 'asc' ? '↓' : '↑'}</span>}
-            </div>
+            每百元期望值{sort === 'expect' && <span>{order === 'asc' ? '↓' : '↑'}</span>}
           </div>
           <div
-            className="flex h-full w-1/12 min-w-[100px] cursor-pointer items-center justify-center"
+            className="box-border flex h-full w-1/12 min-w-[100px] cursor-pointer items-center justify-center bg-red-500 px-1"
             onClick={() => click('topPrize')}
           >
-            <div className="p-1">
-              頭獎獎金{sort === 'topPrize' && <span>{order === 'asc' ? '↓' : '↑'}</span>}
-            </div>
+            頭獎獎金{sort === 'topPrize' && <span>{order === 'asc' ? '↓' : '↑'}</span>}
           </div>
           <div
-            className="flex h-full w-1/12 min-w-[75px] cursor-pointer items-center justify-center"
+            className="box-border flex h-full w-1/12 min-w-[75px] cursor-pointer items-center justify-center bg-red-500 px-1"
             onClick={() => click('topCount')}
           >
-            <div className="p-1">
-              頭獎數量{sort === 'topCount' && <span>{order === 'asc' ? '↓' : '↑'}</span>}
-            </div>
+            頭獎數量{sort === 'topCount' && <span>{order === 'asc' ? '↓' : '↑'}</span>}
           </div>
           <div
-            className="flex h-full w-1/6 min-w-[150px] cursor-pointer items-center justify-center"
+            className="box-border flex h-full w-1/6 min-w-[150px] cursor-pointer items-center justify-center bg-red-500 px-1"
             onClick={() => click('closedAt')}
           >
-            <div className="p-1">
-              下市日{sort === 'closedAt' && <span>{order === 'asc' ? '↓' : '↑'}</span>}
-            </div>
+            下市日{sort === 'closedAt' && <span>{order === 'asc' ? '↓' : '↑'}</span>}
           </div>
         </div>
         {stats?.map((v, i) => (
           <div
             key={v.id}
             className={classNames(
-              'flex items-center text-center min-w-[1000px] max-w-[1440px] mx-auto text-orange-900',
+              'mx-auto box-border flex min-w-[1000px] max-w-[1440px] items-center text-center text-orange-900',
               {
                 'bg-yellow-200': i % 2 === 0,
                 'bg-yellow-100': i % 2 === 1,
@@ -131,7 +111,7 @@ const Home = () => {
           >
             <div
               className={classNames(
-                'w-1/6 min-w-[150px] p-1 font-bold sticky left-0 border-r-[1px] border-r-gray-800 z-20',
+                'sticky left-0 z-20 box-border w-1/6 min-w-[150px] border-r-[1px] border-r-gray-800 p-1 font-bold',
                 {
                   'bg-yellow-200': i % 2 === 0,
                   'bg-yellow-100': i % 2 === 1,
@@ -140,38 +120,38 @@ const Home = () => {
             >
               {v.topic}
             </div>
-            <div className="w-1/12 min-w-[75px] p-1">{v.price}元</div>
-            <div className="w-1/12 min-w-[150px] p-1">
+            <div className="box-border w-1/12 min-w-[75px] p-1">{v.price}元</div>
+            <div className="box-border w-1/12 min-w-[150px] p-1">
               {v.totalW}萬{v.totalR === 0 ? '' : v.totalR}張
             </div>
-            <div className="relative w-1/12 min-w-[75px] p-1">
+            <div className="relative box-border w-1/12 min-w-[75px] p-1">
               <div className="relative z-10">{bn(v.bingoRate).toFixed(2)}%</div>
               <span
                 className="absolute inset-y-0 left-1 my-1 rounded-sm bg-yellow-400 opacity-95"
                 style={{ width: `calc((100% - 8px)*${v.bingoRate / 100})` }}
               />
             </div>
-            <div className="relative w-1/12 min-w-[75px] p-1">
+            <div className="relative box-border w-1/12 min-w-[75px] p-1">
               <div className="relative z-10">{bn(v.winRate).toFixed(2)}%</div>
               <span
                 className="absolute inset-y-0 left-1 my-1 rounded-sm bg-yellow-400 opacity-95"
                 style={{ width: `calc((100% - 8px)*${v.winRate / 50})` }}
               />
             </div>
-            <div className="relative w-1/12 min-w-[75px] p-1">
+            <div className="relative box-border w-1/12 min-w-[75px] p-1">
               <div className="relative z-10">{bn(v.noLoseRate).toFixed(2)}%</div>
               <span
                 className="absolute inset-y-0 left-1 my-1 rounded-sm bg-yellow-400 opacity-95"
                 style={{ width: `calc((100% - 8px)*${v.noLoseRate / 100})` }}
               />
             </div>
-            <div className="w-1/12 min-w-[75px] p-1">{bn(v.expect).toFixed(2)}元</div>
-            <div className="w-1/12 min-w-[100px] p-1">{v.topPrize}萬元</div>
-            <div className="w-1/12 min-w-[75px] p-1">{v.topCount}張</div>
-            <div className="w-1/6 min-w-[150px] p-1">{v.closedAt}</div>
+            <div className="box-border w-1/12 min-w-[75px] p-1">{bn(v.expect).toFixed(2)}元</div>
+            <div className="box-border w-1/12 min-w-[100px] p-1">{v.topPrize}萬元</div>
+            <div className="box-border w-1/12 min-w-[75px] p-1">{v.topCount}張</div>
+            <div className="box-border w-1/6 min-w-[150px] p-1">{v.closedAt}</div>
           </div>
         ))}
-        <div className="mx-6 mt-4 rounded-2xl bg-yellow-50 px-10 py-4">
+        <div className="mx-6 mt-4 box-border max-w-[1440px] rounded-2xl bg-yellow-50 px-10 py-4 xl:mx-auto">
           <h1>刮刮樂機率分析</h1>
           <p>點擊標頭可以排序，尋找最符合自己需求的刮刮樂。</p>
           <p>

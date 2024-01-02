886cd03e7902bc2d30dd72a8d07b0c9faee73651
Yue <yue@potentia.asia>
2024-01-02 16:13:46 +0800

    update crawler

--- crawler/index.js
+++ crawler/index.js
@@ -2,89 +2,62 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const fs = require('fs');
 
-const getDetails = async (id, url) => {
-    const pageHTML = await axios.get(url)
-    const $ = cheerio.load(pageHTML.data)
+const getStructure = async (id, newsId) => {
+    const pageHTML = await axios.get(`https://www.taiwanlottery.com/news/news/${newsId}`)
+    const $0 = cheerio.load(pageHTML.data)
+    const $ = cheerio.load($0('#lotto-news-iframe').attr('src'))
 
     const structure = []
-    let total
+    const table = $(`#${id}`).parent().siblings('table').children('tbody').children().children().children('ul')
+    let n = 0
+    let queue = []
+    for (const ul of $(table)) {
+        const li = $(ul).children('li')
+        n = n + 1
+        if (n % 2 === 1) {
+            $(li).each((i, e) => {
+                if ($(e).text() === '\u00a0') return
+                queue.push(Number($(e).text().split('NT$')[1].replace(/\,/g, '')))
+            })
+        }
+        if (n % 2 === 0) {
+            $(li).each((i, e) => {
+                if ($(e).text() === '\u00a0') return
+                const prize = queue.shift()
+                if (prize === undefined) throw new Error('price is undefined')
+                structure.push({ prize, count: Number($(e).text().replace(/\,/g, '')) })
+            })
+        }
+    }
 
-    const topic = $(`#${id}`).parent().next().text().split('\n')[0].split('：')[1]
-
-    const table = $(`#${id}`).parent().siblings('.tableD').children().children()
-    const length = $(table).length
-    $(table).each((index, element) => {
-        $('td', element).each((i, e) => {
-            if (index !== 0 && index !== length - 1) {
-                if (i % 2 === 0 && $(e).text().trim() !== '') {
-                    const prize = Number($(e).text().split('NT$')[1].replace(/\,/g, ''))
-                    const count = Number($(e.nextSibling.nextSibling).text().replace(/\,/g, ''))
-                    structure.push({ prize, count })
-                }
-            }
-            if (index === length - 1) {
-                if (i === 1) {
-                    total = Number($(e).text().replace(/\,/g, ''))
-                }
-            }
-        })
-    })
-    return { topic, total, structure }
+    return structure
 }
 
-const getOverall = async () => {
-    const listPage = 'https://www.taiwanlottery.com.tw/info/instant/sale.aspx'
-    const pageHTML = await axios.get(listPage)
-    const $ = cheerio.load(pageHTML.data)
-
-    const result = []
-
-    $('.tableFull tbody tr').each((index, element) => {
-        if (element.type !== 'tag') return;
-
-        let price
-        let releaseDate
-        let closeDate
-        if ((index - 1) % 4 === 0) {
-            $('td', element).each((i, e) => {
-                if (i === 1) price = Number($(e).text().replace(/\,/g, ''))
-                if (i === 3) {
-                    const tmpRelease = $(e).text()
-                    const [year, month, day] = tmpRelease.split('/')
-                    releaseDate = new Date(Number(year) + 1911, Number(month - 1), day)
-                }
-                if (i === 4) {
-                    const tmpeClose = $(e).text()
-                    const [year, month, day] = tmpeClose.split('/')
-                    closeDate = new Date(Number(year) + 1911, Number(month - 1), day)
-                }
-            })
-        }
-        if (closeDate > Date.now()) {
-            const url = $('a', element.nextSibling).attr('href')
-            const id = url.split('#')[1]
-            result.push({
-                id, price, releasedAt: releaseDate.toISOString(), closedAt: closeDate.toISOString(), url
-            })
-        }
-    })
-
-    return result
+const getAllScratches = async () => {
+    const res = await axios.get('https://api.taiwanlottery.com/TLCAPIWeB/Instant/Result?ScratchName&Start_ListingDate&End_ListingDate&PageNum=1&PageSize=100&Type=1')
+    return res.data.content.resultList
 }
 
 const main = async () => {
     let result = []
 
     let count = 0;
-    const maxTries = 10;
+    const maxTries = 3;
     while (true) {
         try {
-            const tempResult = await getOverall()
+            const scratches = await getAllScratches()
 
-            for (const i of tempResult) {
-                const res = await getDetails(i.id, i.url)
-                const { url, ...rest } = i
-                result.push({ ...rest, ...res })
+            for (const i of scratches) {
+                const structure = await getStructure(i.gameVol, i.newsId)
+                result.push({
+                    id: i.gameVol,
+                    topic: i.scratchName,
+                    price: i.money,
+                    total: i.issuedCount,
+                    releasedAt: i.listingDate,
+                    closedAt: i.downDate,
+                    structure
+                })
             }
             break;
         } catch (e) {

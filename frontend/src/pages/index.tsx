b4ae0fd877653a6bdf19136a0531e185678177da
yue.chang <lamplighter.planet@gmail.com>
2025-08-03 03:50:27 +0800

    update ld-json

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -99,17 +99,18 @@ const Home = () => {
         </script>
         <script type="application/ld+json">
           {`
-{
-  "@context": "https://schema.org",
-  "@type": "WebPage",
-  "name": "刮刮樂機率分析",
-  "url": "https://lottery.celestialstudio.net/",
-  "description": "最完整的台灣刮刮樂機率分析、勝率、回本率、期望值，每週自動更新。",
-  "inLanguage": "zh-TW",
-  "headline": "刮刮樂機率分析，最容易中獎的是這一個！",
-  "datePublished": ${new Date().toISOString()},
-  "dateModified": ${new Date().toISOString()}
-}
+  {
+      "@context": "https://schema.org",
+      "@type": "NewsArticle",
+      "headline": "刮刮樂機率分析，最容易中獎的是這一個！",
+      "description": "最完整的台灣刮刮樂機率分析、勝率、回本率、期望值，每週自動更新。",
+      "datePublished": ${new Date().toISOString()},
+      "dateModified": ${new Date().toISOString()},
+      "author": [{
+          "@type": "Person",
+          "name": "Celestial Studio"
+        }]
+    }
 `}
         </script>
       </Head>
